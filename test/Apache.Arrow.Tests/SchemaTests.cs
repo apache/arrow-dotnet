e555f186485475ef91c8d131ae37e3c9e886a08c
Vasilis Themelis <vdthemelis@gmail.com>
2026-03-30 23:32:59 +0100

    Use file-scoped namespace in schema tests

--- test/Apache.Arrow.Tests/SchemaTests.cs
+++ test/Apache.Arrow.Tests/SchemaTests.cs
@@ -18,49 +18,48 @@ using System.Collections.Generic;
 using Apache.Arrow.Types;
 using Xunit;
 
-namespace Apache.Arrow.Tests
+namespace Apache.Arrow.Tests;
+
+public class SchemaTests
 {
-    public class SchemaTests
+    [Fact]
+    public void ThrowsWhenFieldsAreNull()
     {
-        [Fact]
-        public void ThrowsWhenFieldsAreNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => new Schema(null, null));
-        }
+        Assert.Throws<ArgumentNullException>(() => new Schema(null, null));
+    }
 
-        [Theory]
-        [MemberData(nameof(StringComparers))]
-        public void CanRetrieveFieldIndexByName(StringComparer comparer)
-        {
-            var field0 = new Field("f0", Int32Type.Default, true);
-            var field1 = new Field("f1", Int64Type.Default, true);
-            var schema = new Schema(new[] { field0, field1 }, null);
+    [Theory]
+    [MemberData(nameof(StringComparers))]
+    public void CanRetrieveFieldIndexByName(StringComparer comparer)
+    {
+        var field0 = new Field("f0", Int32Type.Default, true);
+        var field1 = new Field("f1", Int64Type.Default, true);
+        var schema = new Schema(new[] { field0, field1 }, null);
 
-            Assert.Equal(0, schema.GetFieldIndex("f0", comparer));
-            Assert.Equal(1, schema.GetFieldIndex("f1", comparer));
-            Assert.Equal(-1, schema.GetFieldIndex("nonexistent", comparer));
-        }
+        Assert.Equal(0, schema.GetFieldIndex("f0", comparer));
+        Assert.Equal(1, schema.GetFieldIndex("f1", comparer));
+        Assert.Equal(-1, schema.GetFieldIndex("nonexistent", comparer));
+    }
 
-        [Theory]
-        [MemberData(nameof(StringComparers))]
-        public void CanRetrieveFieldIndexByNonUniqueName(StringComparer comparer)
-        {
-            var field0 = new Field("f0", Int32Type.Default, true);
-            var field1 = new Field("f1", Int64Type.Default, true);
+    [Theory]
+    [MemberData(nameof(StringComparers))]
+    public void CanRetrieveFieldIndexByNonUniqueName(StringComparer comparer)
+    {
+        var field0 = new Field("f0", Int32Type.Default, true);
+        var field1 = new Field("f1", Int64Type.Default, true);
 
-            var schema = new Schema(new[] { field0, field1, field0, field1 }, null);
+        var schema = new Schema(new[] { field0, field1, field0, field1 }, null);
 
-            Assert.Equal(0, schema.GetFieldIndex("f0", comparer));
-            Assert.Equal(1, schema.GetFieldIndex("f1", comparer));
-            Assert.Equal(-1, schema.GetFieldIndex("nonexistent", comparer));
-        }
+        Assert.Equal(0, schema.GetFieldIndex("f0", comparer));
+        Assert.Equal(1, schema.GetFieldIndex("f1", comparer));
+        Assert.Equal(-1, schema.GetFieldIndex("nonexistent", comparer));
+    }
 
-        public static IEnumerable<object[]> StringComparers()
-        {
-            yield return new object[] { null };
-            yield return new object[] { StringComparer.Ordinal };
-            yield return new object[] { StringComparer.OrdinalIgnoreCase };
-            yield return new object[] { StringComparer.CurrentCulture };
-        }
+    public static IEnumerable<object[]> StringComparers()
+    {
+        yield return new object[] { null };
+        yield return new object[] { StringComparer.Ordinal };
+        yield return new object[] { StringComparer.OrdinalIgnoreCase };
+        yield return new object[] { StringComparer.CurrentCulture };
     }
 }
