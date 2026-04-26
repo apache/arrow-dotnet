842d0f7ec1923bd15200d67401b62ca5f5c19eb1
InCerryGit <incerry@foxmail.com>
2026-04-26 23:06:19 +0800

    style: sort StringArray test usings

--- test/Apache.Arrow.Tests/StringArrayTests.cs
+++ test/Apache.Arrow.Tests/StringArrayTests.cs
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Text;
 using System.Collections.Generic;
+using System.Text;
 using Xunit;
 
 namespace Apache.Arrow.Tests
